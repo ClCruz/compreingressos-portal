e2de12fda52b5faa0c1a4d80fe00ebfaf2b15843
Gabriel de Paula Queiroz <gabriel.queiroz@intuiti.com.br>
2016-08-18 09:29:48 -0300

    #241 - Incluindo o layout em algumas páginas

--- src/main/java/br/com/intuiti/compreingressos/portal/controller/CanalVendaController.java
+++ src/main/java/br/com/intuiti/compreingressos/portal/controller/CanalVendaController.java
@@ -19,6 +19,7 @@ import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
+
 @Named("canalVendaController")
 @SessionScoped
 public class CanalVendaController implements Serializable {
--- target/generated-sources/annotations/br/com/intuiti/compreingressos/portal/model/TipoLocal_.java
+++ target/generated-sources/annotations/br/com/intuiti/compreingressos/portal/model/TipoLocal_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-08-10T17:37:55")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-08-17T18:52:59")
 @StaticMetamodel(TipoLocal.class)
 public class TipoLocal_ { 
 
